624fa0f948d2c29ff09567774feb393b99ff8dfb
Renan Zulian <renanzulian@gmail.com>
2020-03-28 22:51:39 -0300

    configurando routing das telas

--- web-map/src/app/app-routing.module.ts
+++ web-map/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { MapComponent } from './components/map/map.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent},
+  { path: '', component: HomeComponent},
+  { path: 'map', component: MapComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- web-map/src/app/app.module.ts
+++ web-map/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { MapComponent } from './components/map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
